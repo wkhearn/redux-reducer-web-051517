61aa54495f67147ea1981695b5f0c9070625b253
wkhearn <wkhearn@gmail.com>
2017-08-02 16:15:11 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+
+  switch (action.type) {
+
+    case "ADD_FRIEND":
+      return state = {friends: [...state.friends, action.friend]}
+      break
+
+    case "REMOVE_FRIEND":
+      const badFriendIndex = state.friends.findIndex((friend) => friend.id === action.id)
+      state = {
+        friends: [
+          ...state.friends.slice(0, badFriendIndex),
+          ...state.friends.slice(badFriendIndex + 1)
+        ]}
+      return state
+      break
+
+    default:
+      return state
+  }
+  
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
 export function managePresents(state, action){
+  switch (action.type) {
+    case "INCREASE":
+      return { numberOfPresents: state.numberOfPresents + 1}
+      break
+    default:
+      return state
+  }
 }
